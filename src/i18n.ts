d6fb596c302d0fa4145c382aa0efe0e4b117c4ef
KindKang <kindkang@alllivesmatter.world>
2024-09-02 10:23:26 +0800

    fix origin: todo: remove ga

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from 'react-i18next';
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? window.location.origin : 'https://multi-lang-app-react.netlify.app';
+const getCurrentHost = import.meta.env.MODE === 'development' ? window.location.origin : 'https://www.alllivesmatter.world';
 
 i18n
     .use(i18nBackend)
