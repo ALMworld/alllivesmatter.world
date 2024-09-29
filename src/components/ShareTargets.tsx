ce530aa1c37b464c57a7fbce5dc114d4dda6cba5
KindKang <kindkang@alllivesmatter.world>
2024-09-29 13:22:00 +0800

    feat: commit first, try add hashtag options for different symbol beliefs

---
+++ src/components/ShareTargets.tsx
@@ -0,0 +1,294 @@
+import {
+    EmailIcon,
+    EmailShareButton,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    FacebookMessengerShareButton,
+    FacebookShareButton,
+    FacebookShareCount,
+    GabIcon,
+    GabShareButton, HatenaIcon,
+    HatenaShareButton,
+    HatenaShareCount,
+    InstapaperIcon,
+    InstapaperShareButton,
+    LineIcon,
+    LineShareButton,
+    LinkedinIcon,
+    LinkedinShareButton,
+    LivejournalIcon,
+    LivejournalShareButton,
+    MailruIcon,
+    MailruShareButton,
+    OKIcon,
+    OKShareButton,
+    OKShareCount,
+    PinterestIcon,
+    PinterestShareButton,
+    PinterestShareCount,
+    PocketIcon,
+    PocketShareButton,
+    RedditIcon,
+    RedditShareButton,
+    RedditShareCount,
+    TelegramIcon,
+    TelegramShareButton,
+    TumblrIcon,
+    TumblrShareButton,
+    TumblrShareCount,
+    TwitterShareButton,
+    ViberIcon,
+    ViberShareButton,
+    VKIcon,
+    VKShareButton,
+    VKShareCount,
+    WeiboIcon,
+    WeiboShareButton,
+    WhatsappIcon,
+    WhatsappShareButton,
+    WorkplaceIcon,
+    WorkplaceShareButton,
+    XIcon
+} from "react-share";
+import * as React from "react";
+import { Box, Share2 } from "lucide-react";
+import { useState } from "react";
+
+
+export default function ShareTargets({ shareUrl, shareContent }) {
+
+    // const shareUrl = 'https://www.alllivesmatter.world';
+    // const shareUrl = data.share_url;
+    // const title = data.share_title;
+    // const title = data.share_advocacy;
+    const exampleImage = "/alm.jpg"
+    // <img src="" alt="ALM" width={64} height={64} />
+    const iconSize = 42;
+
+
+
+    return (
+        <>
+            <div className="share__container bg-slate-600 text-black px-2 py-3 rounded-xl">
+                <div className="share__some-network">
+                    <TwitterShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <XIcon size={iconSize} round />
+                    </TwitterShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <FacebookShareButton url={shareUrl} className="share__some-network__share-button">
+                        <FacebookIcon size={iconSize} round />
+                    </FacebookShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <FacebookMessengerShareButton
+                        url={shareUrl}
+                        appId="521270401588372"
+                        className="share__some-network__share-button"
+                    >
+                        <FacebookMessengerIcon size={iconSize} round />
+                    </FacebookMessengerShareButton>
+                </div>
+
+
+
+                <div className="share__some-network">
+                    <TelegramShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <TelegramIcon size={iconSize} round />
+                    </TelegramShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <WhatsappShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        separator=":: "
+                        className="share__some-network__share-button"
+                    >
+                        <WhatsappIcon size={iconSize} round />
+                    </WhatsappShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <LinkedinShareButton url={shareUrl} className="share__some-network__share-button">
+                        <LinkedinIcon size={iconSize} round />
+                    </LinkedinShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <PinterestShareButton
+                        url={String(window.location)}
+
+                        media={`${String(window.location)}/${exampleImage}`}
+                        className="share__some-network__share-button"
+                    >
+                        <PinterestIcon size={iconSize} round />
+                    </PinterestShareButton>
+
+                </div>
+
+                <div className="share__some-network">
+                    <VKShareButton
+                        url={shareUrl}
+                        image={`${String(window.location)}/${exampleImage}`}
+                        className="share__some-network__share-button"
+                    >
+                        <VKIcon size={iconSize} round />
+                    </VKShareButton>
+
+                </div>
+
+                <div className="share__some-network">
+                    <OKShareButton
+                        url={shareUrl}
+                        image={`${String(window.location)}/${exampleImage}`}
+                        className="share__some-network__share-button"
+                    >
+                        <OKIcon size={iconSize} round />
+                    </OKShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <RedditShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        windowWidth={660}
+                        windowHeight={460}
+                        className="share__some-network__share-button"
+                    >
+                        <RedditIcon size={iconSize} round />
+                    </RedditShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <GabShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        windowWidth={660}
+                        windowHeight={640}
+                        className="share__some-network__share-button"
+                    >
+                        <GabIcon size={iconSize} round />
+                    </GabShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <TumblrShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <TumblrIcon size={iconSize} round />
+                    </TumblrShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <LivejournalShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        description={shareUrl}
+                        className="share__some-network__share-button"
+                    >
+                        <LivejournalIcon size={iconSize} round />
+                    </LivejournalShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <MailruShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <MailruIcon size={iconSize} round />
+                    </MailruShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <EmailShareButton
+                        url={shareUrl}
+                        subject={shareContent}
+                        body="body"
+                        className="share__some-network__share-button"
+                    >
+                        <EmailIcon size={iconSize} round />
+                    </EmailShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <ViberShareButton url={shareUrl} title={shareContent} className="share__some-network__share-button">
+                        <ViberIcon size={iconSize} round />
+                    </ViberShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <WorkplaceShareButton
+                        url={shareUrl}
+                        quote={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <WorkplaceIcon size={iconSize} round />
+                    </WorkplaceShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <LineShareButton url={shareUrl} title={shareContent} className="share__some-network__share-button">
+                        <LineIcon size={iconSize} round />
+                    </LineShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <WeiboShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        image={`${String(window.location)}/${exampleImage}`}
+                        className="share__some-network__share-button"
+                    >
+                        <WeiboIcon size={iconSize} round />
+                    </WeiboShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <PocketShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <PocketIcon size={iconSize} round />
+                    </PocketShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <InstapaperShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        className="share__some-network__share-button"
+                    >
+                        <InstapaperIcon size={iconSize} round />
+                    </InstapaperShareButton>
+                </div>
+
+                <div className="share__some-network">
+                    <HatenaShareButton
+                        url={shareUrl}
+                        title={shareContent}
+                        windowWidth={660}
+                        windowHeight={460}
+                        className="share__some-network__share-button"
+                    >
+                        <HatenaIcon size={iconSize} round />
+                    </HatenaShareButton>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/WillGallery.tsx
@@ -0,0 +1,190 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { ChevronLeft, ChevronRight, X } from 'lucide-react';
+import Slider from 'react-slick';
+import { HisWillAndOurGoodFreeWill } from '../data/data_types';
+import { useGallery } from '../data/gallery_provider';
+import { UnitFile } from '../data/gallery_types';
+
+// Note: You need to import the CSS for react-slick and its default theme
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { createPortal } from 'react-dom';
+import ImageGalleryModal from './ImageGalleryModal';
+
+
+type DeliveryState = 'unknown' | 'delivered';
+
+interface WillWithState extends HisWillAndOurGoodFreeWill {
+  deliveryState: DeliveryState;
+  unitFile: UnitFile | null;
+}
+
+interface WillGalleryProps {
+  text_wills_list: HisWillAndOurGoodFreeWill[];
+  media_wills_list: HisWillAndOurGoodFreeWill[];
+}
+
+const WillGallery: React.FC<WillGalleryProps> = ({ text_wills_list, media_wills_list }) => {
+  const [selectedImage, setSelectedImage] = useState<number | null>(null);
+  const { loadingState, error, getImageFiles } = useGallery();
+
+  const thanksLettersWithState: WillWithState[] = useMemo(() => {
+    const imageKeys = text_wills_list.map(will => will.free_will);
+    const unitFiles = loadingState === 'loaded' ? getImageFiles(imageKeys) : [];
+    return text_wills_list.map((will, index) => ({
+      ...will,
+      deliveryState: Math.random() < 0.5 ? 'unknown' : 'delivered',
+      unitFile: unitFiles[index] || null
+    }));
+  }, [text_wills_list, loadingState, getImageFiles]);
+
+  const thanksLettersImages: UnitFile[] = useMemo(() => {
+    const imageKeys = text_wills_list.map(will => will.free_will);
+    const unitFiles = loadingState === 'loaded' ? getImageFiles(imageKeys) : [];
+    return text_wills_list.map((will, index) =>
+      unitFiles[index]
+    );
+  }, [text_wills_list, loadingState, getImageFiles]);
+
+
+  const images = useMemo(() => {
+    // If selectedImage is just a single image object, we wrap it in an array
+    return selectedImage ? [selectedImage] : [];
+  }, [selectedImage]);
+
+  const openImage = (index: number) => {
+    setSelectedImage(index);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+  };
+
+
+  const thanksLetterSliderSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+        }
+      }
+    ]
+  };
+
+  const sliderSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  };
+
+  if (loadingState === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (loadingState === 'error') {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="w-full max-w-4xl mx-auto">
+      <div>
+        <div className="bg-[#d20033] rounded-t-lg mt-2 flex items-center" style={{ minHeight: '150px' }}>
+          <Slider {...thanksLetterSliderSettings} className="w-full">
+            {thanksLettersWithState.map((will, index) => (
+              <div key={index} className="px-2">
+                <div className="h-[120px] rounded-lg cursor-pointer relative">
+                  {will.unitFile && (
+                    <>
+                      <img
+                        src={will.unitFile.file_uri}
+                        alt={will.will_summary}
+                        className="w-full h-[100px] object-contain hover:opacity-80 transition-opacity"
+                        onClick={() => openImage(index)}
+                      />
+                      <div className="absolute bottom-[-1] left-0 right-0 text-center text-sm text-white py-1 text-yellow-300">
+                        {will.from}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+
+        <div >
+          <Slider {...sliderSettings} className="bg-[#d20033] rounded-b-lg  ">
+            {media_wills_list.map((will, index) => {
+              const videoId = will.free_will
+              return (
+                <div key={index} className="px-2">
+                  <div className="aspect-video overflow-hidden rounded-lg">
+                    {videoId ? (
+                      <>
+                        <iframe
+                          width="100%"
+                          height="100%"
+                          src={`https://www.youtube.com/embed/${videoId}`}
+                          title={`YouTube video player ${index + 1}`}
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                          loading="lazy"
+                        ></iframe>
+
+                      </>
+                    ) : (
+                      <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+                        <span className="text-gray-500">Invalid video URL</span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="min-h-[60px] p-2 rounded">
+                    <p className="text-yellow-300 text-sm break-words">
+                      {will.will_summary}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+      </div>
+
+      {selectedImage != null && <ImageGalleryModal images={thanksLettersImages} isOpen={openImage} onClose={closeImage} initialIndex={selectedImage} />}
+
+
+    </div>
+  );
+};
+
+export default WillGallery;
